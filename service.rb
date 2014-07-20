5ad216eae45d1e947eecd7041d96d1b5366c3527
bwidtmann <bw@12auto.de>
2014-07-20 18:13:12 +0200

    integrate rabbitmq over bigwig heroku addon

--- service.rb
+++ service.rb
@@ -7,8 +7,8 @@ configure do
 end
 
 require_relative 'customer.rb'
-#require_relative 'sender.rb'
-#require_relative 'receiver.rb'
+require_relative 'sender.rb'
+require_relative 'receiver.rb'
 
 # start listening on server queue
-#Receiver.new.receive
+Receiver.new.receive
